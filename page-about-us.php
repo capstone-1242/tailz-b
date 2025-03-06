c3db46d0869a5d2ae3d5f258ec4c88c6c5fd9cd4
FzySideUp <brandonsimyeg@gmail.com>
2025-03-06 15:04:33 -0700

    footer

--- page-about-us.php
+++ page-about-us.php
@@ -2,17 +2,56 @@
 
 <div class="container mx-auto my-8">
 
-	<?php if ( have_posts() ) : ?>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
+<div class="flex flex-col">
+    <!-- Page heading -->
+        <div>
+            <h2 class="uppercase font-bold">About Us</h2>
+            <p class="uppercase font-bold">Pets are our passion</p>
+        </div>
 
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+    <!-- Page Content -->
+        <div class="page-content">
+            <?php
+                if (have_posts()) :
+                    while (have_posts()) : the_post();
+                        the_content();
+                    endwhile;
+                endif;
+            ?>
+        </div>
 
-		<?php endwhile; ?>
-
-	<?php endif; ?>
+    <!-- Staff Post Type Query Loop -->
+        <div>
+            <h3 class="uppercase font-bold">Meet Our Team</h3>
+        </div>
+        <div class="grid grid-cols-2 md:grid-cols-3">
+            <?php
+                    $staff_query = new WP_Query(array(
+                        'post_type' => 'staff',
+                        'posts_per_page' => -1,
+                    ));
+    
+                    if($staff_query->have_posts()) :
+                        while($staff_query->have_posts()) : $staff_query->the_post(); ?>
+    
+                            <div>
+                                <?php if(has_post_thumbnail()) : ?>
+                                    <?php the_post_thumbnail('medium'); ?>
+                                <?php endif; ?>
+                                <?php the_title(); ?>
+                                <?php the_content();  ?>
+                            </div>
+    
+    
+                        <?php
+                        endwhile;
+                        wp_reset_postdata();
+                    else :
+                        echo '<p>No staff members found.</p>';
+                    endif;
+                ?>
+        </div>
+</div>
 
 </div>
 
