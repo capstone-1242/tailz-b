26cb0074f6a8961b261d5565e336cf20e4b96bc1
FzySideUp <144250678+FzySideUp@users.noreply.github.com>
2025-04-04 02:25:59 -0600

    training layout

--- page-training.php
+++ page-training.php
@@ -10,8 +10,7 @@ get_header();
 ?>
 
 <div class="container mx-auto">
-
-    <!-- Page Content -->
+    <!-- Banner -->
     <section class="flex flex-col gap-3">
         <div class="relative h-[15.375rem] w-full overflow-hidden">
             <?php
@@ -25,58 +24,80 @@ get_header();
                     <p class="uppercase text-white text-[18px]">Lessons for every type of pup</p>
                 </div>            
         </div>
-
+    </section>
+    
+    <!-- The leash is in your hands, we'll show you how to lead -->
+    <section>
         <div class="flex flex-col mx-6 gap-3">
             <h3 class="lowercase font-bold text-[22px] text-[#615849]">The leash is in your hands, we'll show you how to lead</h3>
             <p class="text-[18px] text-[#47423B]">When a dog's behavior falls outside the "normal" range, it often requires extra guidance—not just from skilled trainers, but from owners committed to their companion's growth.</p>
             <p class="text-[18px] text-[#47423B]"><span class="font-bold">First, let's drop the stigma.</span> Asking for help is not just okay—it's a sign of dedication. Training isn't a quick fix; it's a lifestyle and a lifelong partnership. To create lasting change, we focus on <span class="font-bold">THE WHY</span> behind your pup's behavior, so you can master <span class="font-bold">THE HOW</span> to support them effectively.</p>
         </div>
-
-        <div class="flex-col mx-6 gap-3">
-            <h3 class="lowercase font-bold text-[22px] text-[#615849]">Understanding canine behavior and skill building</h3>
-            <h4 class="lowercase text-[22px] text-[#837660]">Your role in the process</h4>
-            <p class="text-[18px] text-[#47423B]">During Canine Skill Building, we start by defining your goals. Every household is different, and success looks unique for each dog. Ask yourself:</p>
-            <ol class="flex-col gap-3 text-[18px] text-[#615849] font-bold">
-                <li>How can I help my dog reach these goals?</li>
-                <li>What time and effort am I willing to invest?</li>
-            </ol>
-            <p class="text-[18px] text-[#47423B]">These choices shape the outcome. This isn't just "training" - it's a transformative learning journey for both you and your pup. You'll gain systematic tools to help your pup develop the skills they need to thrive.</p>
-            <h3 class="lowercase text-[22px] text-[#837660]">Our commitment to you</h3>
-            <p>After your initial consultation, our trainer will craft a personalized plan for ongoing skill development, including:</p>
-            <ol class="flex-col gap-3 text-[18px] text-[#615849] font-bold">
-                <li>One-on-one coaching</li>
-                <li>Structured exercise</li>
-                <li>Real world practice</li>
-            </ol>
-        </div>
-
-
-
-        <!-- <div class="flex flex-col mx-6 gap-3">
-            <h3 class="lowercase font-bold text-[22px] text-[#615849]">Understanding canine behavior and skill building</h3>
-            <p class="text-[18px] text-[#47423B]">During the Canine Skill Building process, we need to figure out your goals for your pup. Two different households can be looking for different results. Ultimately, we want you to able to answer a few fundamental questions:</p>
-            <ol class="flex flex-col text-[18px] text-[#47423B] italic gap-3">
-                <li>How do I help them achieve my goal for them?</li>
-                <li>How much time and effort am I willing to commit to their success?</li>
-            </ol>
-            <p class="text-[18px] text-[#47423B]">All these factors affect the outcome. Canine Skill building is not just training - it's a learning process for both you and your pup. Throughout the process, you will be systematically given tools to help your pup build the skills they need to improve their behaviour.</p>
-            <p class="text-[18px] text-[#47423B]">Training happens through repetition and with the Skill Building, learning occurs as a sensory experience. It is more holistic - one in which all your dog's senses are activated in a rich and robust way. Skill Building will deepen your relationship with your dog.</p>
-            <p class="text-[18px] text-[#47423B]">After your initial consultation, our trainer will thoughtfully develop a plan for ongoing skill enhances, structure and one-on-one support.</p>
-            <p class="text-[18px] text-[#47423B]">Some common Canine Skill Building Includes:</p>
-            <ol class="flex flex-col text-[18px] text-[#47423B] italic gap-3">
-                <li>Leash reactivity</li>
-                <li>Reactivity (other dogs or humans)</li>
-                <li>Barrier frustration (ie. fence fighting)</li>
-                <li>Excessive barking</li>
-                <li>Door dashing</li>
-                <li>Resource guarding</li>
-                <li>Fearfulness (dogs or humans)</li>
-                <li>Poor leash manners</li>
-                <li>Confidence issues</li>
-            </ol>
-        </div> -->
     </section>
 
+    <!-- Understanding canine behavior and skill building -->
+    <section>
+        <div class="flex-col mx-6 gap-3">
+            <h2 class="lowercase font-bold text-[22px] text-[#615849]">Understanding canine behavior and skill building</h2>
+            <!-- Content Container -->  
+            <div class="flex-col gap-15">
+                <!-- Container 1 -->
+                <div class="flex-col gap-15">
+                    <!-- Your role in the process -->
+                    <div class="flex-col gap-3">
+                        <h3 class="lowercase text-[22px] text-[#837660]">Your role in the process</h3>
+                        <p class="text-[18px] text-[#47423B]">During Canine Skill Building, we start by defining your goals. Every household is different, and success looks unique for each dog. Ask yourself:</p>
+                        <ol class="flex-col gap-3 text-[18px] text-[#615849] font-bold">
+                            <li>How can I help my dog reach these goals?</li>
+                            <li>What time and effort am I willing to invest?</li>
+                        </ol>
+                        <p class="text-[18px] text-[#47423B]">These choices shape the outcome. This isn't just "training" - it's a transformative learning journey for both you and your pup. You'll gain systematic tools to help your pup develop the skills they need to thrive.</p>
+                    </div>
+                    <!-- Our commitment to you -->
+                    <div class="flex-col gap-3">
+                        <h3 class="lowercase text-[22px] text-[#837660]">Our commitment to you</h3>
+                        <p class="text-[18px] text-[#47423B]">After your initial consultation, our trainer will craft a personalized plan for ongoing skill development, including:</p>
+                        <ol class="flex-col gap-3 text-[18px] text-[#615849] font-bold">
+                            <li>One-on-one coaching</li>
+                            <li>Structured exercise</li>
+                            <li>Real world practice</li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- Container 2 -->
+                <div class="flex-col gap-15">
+                    <!-- Why skillbuilding works -->
+                    <div class="flex-col gap-3">
+                        <img src="" alt="">
+                        <h3 class="lowercase text-[22px] text-[#837660]">Why skill building works</h3>
+                        <p class="text-[18px] text-[#47423B]">True learning happens through repetition and sensory experience. Our approach is holistic, engaging your dog's mind, body, and instincts in a rich, structured way. Beyond correcting behaviors, skill building strengthens your bond and builds confidence.</p>
+                    </div>
+                    <!-- Common skill building focus areas -->
+                    <div class="flex-col gap-3">
+                        <h3>Common skill building focus areas</h3>
+                        <ol class="flex-col md:flex-row gap-3 text-[18px] text-[#47423B]">
+                            <div class="flex-col gap-3 mb-3 md:mb-0">
+                                <li>Leash Reactivity</li>
+                                <li>Dog Reactivity</li>
+                                <li>Human Reactivity</li>
+                                <li>Featfulness</li>
+                                <li>Resource Guarding</li>
+                            </div>
+                            <div class="flex col gap-3">
+                                <li>Barrier Frustration</li>
+                                <li>Door Dash</li>
+                                <li>Excessive Barking</li>
+                                <li>Poor Leash Manners</li>
+                                <li>Confidence Building</li>
+                            </div>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- MetaBox Queries -->
     <?php
     // -----------------------------
     // Retrieve cloned meta values
@@ -154,25 +175,11 @@ get_header();
     }
     ?>
 
-    <div class="container mx-auto px-4 py-6">
-        <!-- Page Heading -->
-        <section class="mb-6">
-            <h1 class="text-2xl font-extrabold mb-2">TRAINING PACKAGES</h1>
-            <p class="text-sm text-gray-700 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam malesuada consectetur justo.
-            </p>
-        </section>
 
-        <!-- Render the single DOG tab with the packages from Meta Box -->
-        <?php render_training_packages( $packages ); ?>
+    <!-- Render the single DOG tab with the packages from Meta Box -->
+    <?php render_training_packages( $packages ); ?>
 
-        <h3 class="mt-6 font-bold">Book some skillz for your pup today!</h3>
-        <button class="border border-black text-black text-sm font-bold px-4 py-2 rounded-full shadow-md hover:bg-black hover:text-white transition">
-            Book Now
-        </button>
-        <p class="mt-2 text-sm">Services must be pre-paid prior to commencing. Canine Skill Building sessions are to be scheduled in advance.</p>
-        <p class="text-sm">We require a minimum of 24-hour notice for rescheduling any Canine Skill Building sessions - not doing so will result in a $50 late cancellation fee or use of one session from the bundle.</p>
-    </div>
+
 
     <section class="px-4 py-6">
         <div>
@@ -215,6 +222,7 @@ get_header();
         </div>
     </section>
 
-</div><!-- /.container -->
+</div>
+<!-- /.container -->
 
 <?php get_footer(); ?>
