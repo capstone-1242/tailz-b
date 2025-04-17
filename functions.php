2e0a7630241ddbf5a2eb02f7bf41c60a7c46e841
Manjot <manjot@example.com>
2025-04-16 18:32:29 -0600

    Add back-to-top functionality with proper script enqueuing

--- functions.php
+++ functions.php
@@ -118,6 +118,15 @@ function tailpress_enqueue_assets()
 		$version,
 		true
 	);
+
+	// Enqueue back to top script
+	wp_enqueue_script(
+		'tailz-back-to-top',
+		get_template_directory_uri() . '/resources/js/back-to-top.js',
+		array(),
+		$version,
+		true
+	);
 }
 add_action('wp_enqueue_scripts', 'tailpress_enqueue_assets');
 
