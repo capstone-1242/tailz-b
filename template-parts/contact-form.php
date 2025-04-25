a3295c37c66a0adedea5053450b369dc7543bef5
Manjot <manjot@example.com>
2025-04-24 18:46:30 -0600

    Updated Contact Form 7 shortcode with new ID and title

--- template-parts/contact-form.php
+++ template-parts/contact-form.php
@@ -9,7 +9,7 @@ $form_id = $args['form_id'] ?? 'default';
 ?>
 
 <div class="relative bg-[#F3F2EC] rounded-[30px] pt-20 pb-8 px-4 sm:px-6 md:p-12">
-  <?php
-    echo do_shortcode( '[contact-form-7 id="489d8ab" html_id="contact-form-'. esc_attr($form_id) .'" html_class="wpcf7-form space-y-6"]' );
-  ?>
+  <div class="space-y-6">
+    <?php echo do_shortcode('[contact-form-7 id="2741b84" title="Tailz Contact Form" html_class="wpcf7-form space-y-6"]'); ?>
+  </div>
 </div>  
